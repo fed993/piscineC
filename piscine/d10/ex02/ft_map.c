f6cf3c5c0a95b4e1031d253f2a0fac5d45dbfbf0
Fedor Polyanskiy <fpolyans@e1z1r4p39.42.us.org>
2017-07-12 13:48:36 -0700

    update post corrections d10

--- piscine/d10/ex02/ft_map.c
+++ piscine/d10/ex02/ft_map.c
@@ -6,11 +6,13 @@
 /*   By: fpolyans <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/11 02:51:12 by fpolyans          #+#    #+#             */
-/*   Updated: 2017/07/11 04:44:05 by fpolyans         ###   ########.fr       */
+/*   Updated: 2017/07/11 22:39:13 by fpolyans         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		*ft_map(int *tab, int length, void (*f)(int))
+#include <stdlib.h>
+
+int		*ft_map(int *tab, int length, int (*f)(int))
 {
 	int		i;
 	int		*out;
