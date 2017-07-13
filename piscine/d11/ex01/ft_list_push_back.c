163e569e5f26d0c4da9d16450d4a9c2be9aa0c15
Fedor Polyanskiy <fpolyans@e1z1r4p39.42.us.org>
2017-07-12 22:17:56 -0700

    d11/ex01 ready for debug

---
+++ piscine/d11/ex01/ft_list_push_back.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_push_back.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpolyans <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/12 20:53:30 by fpolyans          #+#    #+#             */
+/*   Updated: 2017/07/12 22:17:16 by fpolyans         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "ft_list.h"
+#include <stdlib.h>
+
+void	ft_list_push_back(t_list **begin_list, void *data)
+{	
+	t_list	*end_link;
+
+	while ((**begin_list).next != 0)
+	{
+		(*begin_list) = (**begin_list).next;
+	}
+	end_link = ft_create_elem(data);
+}
+
+t_list* ft_create_elem(void* data)
+{
+	t_list* item = malloc(sizeof(t_list));
+	item->next = NULL;
+	item->data = data;
+	return (item);
+}
+
+void crap_a_list(t_list* list)
+{
+	for (; list != 0; list = list->next)
+		printf("%s,", (char*)list->data);
+	printf("\n");
+}
+
+int main()
+{
+	t_list* list = 0;
+	ft_list_push_back(&list, "asdf");
+	crap_a_list(list);
+	ft_list_push_back(&list, "qwer");
+	ft_list_push_back(&list, "zxcv");
+	crap_a_list(list);
+	list = 0;
+	ft_list_push_back(&list, "uiop");
+	ft_list_push_back(&list, "hjkl");
+	crap_a_list(list);
+}
