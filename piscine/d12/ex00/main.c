0e0bf00f0a9b78f24da11a7d54aaf38c4cc39329
EC2 Default User <ec2-user@ip-172-31-17-215.ec2.internal>
2017-07-14 03:56:31 +0000

    completion of d12/ex00

--- piscine/d12/ex00/main.c
+++ piscine/d12/ex00/main.c
@@ -12,20 +12,38 @@
 
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 void	ft_putchar(char c);
 void	ft_putstr(char *c);
+void	ft_putnbr(int i);
+
+#define MB_BUF_SIZE 1048576
+#define KB_BUF_SIZE 1024
 
 int		main(int ac, char **av)
 {
-	int fd;
+	int		fd;
+	int		buf_loc;
+	char	buffer[((KB_BUF_SIZE) * 4) + 1];
 
-	fd = open(av[ac], O_RDWR);
+	fd = open(av[ac - 1], O_RDWR);
 	if (fd == -1)
 	{
-		ft_pustr("open() failed");
+		ft_putstr("open() failed\n");
 		return(1);
 	}
-	ft_putnbr(fd);
+	buf_loc = read(fd, buffer, KB_BUF_SIZE); 
+	while (buf_loc)
+	{
+		buffer[buf_loc] = '\0';
+		ft_putstr(buffer);
+		buf_loc = read(fd, buffer, KB_BUF_SIZE);
+	}
+	if (buf_loc == -1)
+	{
+		ft_putstr("read() failed\n");
+		return (1);
+	}
 	return 0;
 }
