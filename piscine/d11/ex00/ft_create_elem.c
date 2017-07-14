e4cba8f8dd3b505d44e453f86cf8204f8c60b3e9
Fedor Polyanskiy <fpolyans@e1z1r7p3.42.us.org>
2017-07-13 18:57:37 -0700

    creation of d12

--- piscine/d11/ex00/ft_create_elem.c
+++ piscine/d11/ex00/ft_create_elem.c
@@ -6,7 +6,7 @@
 /*   By: fpolyans <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/12 17:17:14 by fpolyans          #+#    #+#             */
-/*   Updated: 2017/07/12 20:43:49 by fpolyans         ###   ########.fr       */
+/*   Updated: 2017/07/13 01:11:07 by fpolyans         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list		*ft_create_elem(void *data)
 {
 	t_list	*list;
 
-	list = malloc(sizeof(t_list));
+	list = (t_list*)malloc(sizeof(t_list));
 	(*list).data = data;
 	(*list).next = 0;
 	return (list);
