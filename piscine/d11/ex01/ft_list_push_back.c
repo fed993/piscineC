77ee3100f97ac3ed61be5bd8492a875055d9db63
Fedor Polyanskiy <fed993@gmail.com>
2017-07-26 22:14:24 -0700

    completion of d11/ex01

--- piscine/d11/ex01/ft_list_push_back.c
+++ piscine/d11/ex01/ft_list_push_back.c
@@ -1,48 +1,22 @@
 #include "ft_list.h"
-#include <stdlib.h>
-#include <stdio.h>
-
-t_list* ft_create_elem(void* data)
-{
-		t_list* item = malloc(sizeof(t_list));
-		item->next = NULL;
-		item->data = data;
-		return (item);
-}
 
 void	ft_list_push_back(t_list **begin_list, void *data)
 {
 		t_list	*link;
 
 		link = ft_create_elem(data);
-		while(((*begin_list)->next) != 0)
+		if(*begin_list == NULL)
 		{
-				(*begin_list) = (*begin_list)->next;
+				*begin_list = link;
+				link->data = data;
 		}
-		(*begin_list)->next = link;
-		link->data = data;
-}
-
-void crap_a_list(t_list* list)
-{
-		for (; list != 0; list = list->next)
-				printf("%s,", (char*)list->data);
-		printf("\n");
-}
-
-int main()
-{
-		t_list	*list;
-		void	*data;
-
-		list = ft_create_elem(data);
-		ft_list_push_back(&list, "asdf");
-		crap_a_list(list);
-		ft_list_push_back(&list, "qwer");
-		ft_list_push_back(&list, "zxcv");
-		crap_a_list(list);
-		list = 0;
-		ft_list_push_back(&list, "uiop");
-		ft_list_push_back(&list, "hjkl");
-		crap_a_list(list);
+		else
+		{
+				while(((*begin_list)->next) != NULL)
+				{
+						(*begin_list) = (*begin_list)->next;
+				}
+				(*begin_list)->next = link;
+				link->data = data;
+		}
 }
